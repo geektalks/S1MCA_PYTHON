3c423e06e98d988e902c85a4cbfb82d2c0cde964
Joel Joseph P <98268075+geektalks@users.noreply.github.com>
2024-01-30 20:30:51 +0530

    Create Multiplication table.py

---
+++ Multiplication table.py
@@ -0,0 +1,8 @@
+num=int(input("Enter the number: "))
+n=int(input("Enter the limit: "))
+print("Multiplication table for ",num,"is: ")
+i=1
+while(i<=n):
+    t=num*i
+    print(num,"*",i,"=",t)
+    i=i+1
