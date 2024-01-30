4119ffafdf3c3b1a160377ad16479753662454cb
Joel Joseph P <98268075+geektalks@users.noreply.github.com>
2024-01-30 23:10:56 +0530

    Create find area and perimeter using lambda.py

---
+++ find area and perimeter using lambda.py
@@ -0,0 +1,15 @@
+print("Rectangle")
+print(".........")
+l=int(input("Enter length of rectangle "))
+b=int(input("Enter breadth of rectangle "))
+area=lambda l,b:l*b
+print("Area of rectangle is ",area(l,b))
+per=lambda l,b:2*(l+b)
+print("Perimeter of rectangle is ",per(l,b)) 
+print("Square")
+print(".........")
+a=int(input("Enter length of one side of square "))
+area=lambda a:a*a
+print(" Area of square is ",area(a))
+per=lambda a:4*a
+print("Perimeter of square is ",per(a)) 
