2d9562b38f00faf785d1dee9d21daa5c7c57ff95
Joel Joseph P <98268075+geektalks@users.noreply.github.com>
2024-01-30 23:05:21 +0530

    Create student list using dictionary.py

---
+++ student list using dictionary.py
@@ -0,0 +1,8 @@
+student={}
+n=int(input("Enter no:of students:"))
+for i in range(0,n):
+ name=input("Enter name:")
+ age=input("Enter age:")
+ grade=input("Enter Grade:")
+ student[name]=age,grade
+print(student)
